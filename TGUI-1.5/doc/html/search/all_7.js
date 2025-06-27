944fc38be8b24412886b39f5caf30c08295d1591
crazyhamster889 <49529327+crazyhamster889@users.noreply.github.com>
2025-06-27 22:47:15 +0100

    Final_Commit

---
+++ TGUI-1.5/doc/html/search/all_7.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['h_0',['H',['../structtgui_1_1_event.html#a647c4342d425d6f03185126fc6eb5cd9ac1d9f50f86825a1a2302ec2449c17196',1,'tgui::Event']]],
+  ['hand_1',['Hand',['../classtgui_1_1_cursor.html#a4e9a3c57acd4bf2b261aa2fe77f4d2f5aa78b1ac16c0cd02168097fc9a9bd7604',1,'tgui::Cursor']]],
+  ['handlecharpressed_2',['handleCharPressed',['../classtgui_1_1_backend_gui_raylib.html#acb5c41fdf400e6f31c3321a8b638c03f',1,'tgui::BackendGuiRaylib']]],
+  ['handleevent_3',['handleEvent',['../classtgui_1_1_backend_gui.html#aa9aeccb7ef5164f64cfb7a91307a0bb4',1,'tgui::BackendGui::handleEvent()'],['../classtgui_1_1_backend_gui_s_d_l.html#a9b24c1126b3d4fc2cc4012070ecb498a',1,'tgui::BackendGuiSDL::handleEvent()'],['../classtgui_1_1_backend_gui_s_f_m_l.html#a2e9338d9f4c2577c6858baefa01f98b7',1,'tgui::BackendGuiSFML::handleEvent()'],['../classtgui_1_1_backend_gui_s_d_l.html#aa9aeccb7ef5164f64cfb7a91307a0bb4',1,'tgui::BackendGuiSDL::handleEvent()'],['../classtgui_1_1_backend_gui_s_f_m_l.html#aa9aeccb7ef5164f64cfb7a91307a0bb4',1,'tgui::BackendGuiSFML::handleEvent()']]],
+  ['handleevents_4',['handleEvents',['../classtgui_1_1_backend_gui_raylib.html#a8a092e204eb9f28b15f3e71ab5eeb39a',1,'tgui::BackendGuiRaylib']]],
+  ['handlekeypressed_5',['handleKeyPressed',['../classtgui_1_1_backend_gui_raylib.html#a5a8db113d51022b1886fd54f5bf60851',1,'tgui::BackendGuiRaylib']]],
+  ['hasfontbackend_6',['hasFontBackend',['../classtgui_1_1_backend.html#afe99cb4abc009a1fc828aa30e6c70fc6',1,'tgui::Backend']]],
+  ['hasgenericicons_7',['hasGenericIcons',['../classtgui_1_1_file_dialog_icon_loader.html#a2b00c9ffeb5ceac7b8649cb03e0ab85f',1,'tgui::FileDialogIconLoader']]],
+  ['hasglyph_8',['hasGlyph',['../classtgui_1_1_backend_font.html#a21325c4f9c59d7199a2a940aabcf5fa4',1,'tgui::BackendFont::hasGlyph()'],['../classtgui_1_1_backend_font_freetype.html#a12ca107e4824cfbae2833eca3e3f1f9d',1,'tgui::BackendFontFreetype::hasGlyph()'],['../classtgui_1_1_backend_font_s_d_lttf.html#a206ac50ae994579aaeb7e21f0bf26268',1,'tgui::BackendFontSDLttf::hasGlyph()'],['../classtgui_1_1_backend_font_s_f_m_l.html#a878b0a39d251f6b709031396069ae0c4',1,'tgui::BackendFontSFML::hasGlyph()'],['../classtgui_1_1_backend_font_raylib.html#a87f1e7567c0fc7c59885e0ff101e734b',1,'tgui::BackendFontRaylib::hasGlyph()']]],
+  ['hasrenderer_9',['hasRenderer',['../classtgui_1_1_backend.html#ab0910bd525cd785fdd9755c2ec25dc80',1,'tgui::Backend']]],
+  ['hasuserdata_10',['hasUserData',['../classtgui_1_1_widget.html#ac24adc1a6bcdbee92123283c215a24bc',1,'tgui::Widget']]],
+  ['height_11',['height',['../structtgui_1_1_event_1_1_size_event.html#a4766bf0b51a5eea70a31f1eeb2cc4eb4',1,'tgui::Event::SizeEvent::height'],['../classtgui_1_1_rect.html#a609ba0a2a45fbe46b8c4176cdb68a23e',1,'tgui::Rect::height'],['../structtgui_1_1_backend_font_freetype_1_1_row.html#a5eb90f2758becd5f744aab816d88af6c',1,'tgui::BackendFontFreetype::Row::height'],['../structtgui_1_1_backend_font_raylib_1_1_row.html#a8d3198da27e088348c44904f140e7a50',1,'tgui::BackendFontRaylib::Row::height']]],
+  ['help_12',['Help',['../classtgui_1_1_cursor.html#a4e9a3c57acd4bf2b261aa2fe77f4d2f5a6a26f548831e6a8c26bfbbd9f6ec61e0',1,'tgui::Cursor']]],
+  ['hidewitheffect_13',['hideWithEffect',['../classtgui_1_1_widget.html#afacd2fa30e6e2b15ac2234ec581e3262',1,'tgui::Widget']]],
+  ['home_14',['Home',['../structtgui_1_1_event.html#a647c4342d425d6f03185126fc6eb5cd9a8cf04a9734132302f96da8e113e80ce5',1,'tgui::Event']]],
+  ['horizontal_15',['Horizontal',['../classtgui_1_1_sprite.html#a3ebf7132d60f5c6f4ca0c3ebd60ea9f8ac1b5fa03ecdb95d4a45dd1c40b02527f',1,'tgui::Sprite::Horizontal'],['../namespacetgui.html#a9a7c068f68617b08f16f85eabb6ddb96ac1b5fa03ecdb95d4a45dd1c40b02527f',1,'tgui::Horizontal']]],
+  ['horizontalalignment_16',['HorizontalAlignment',['../classtgui_1_1_label.html#a0078a0496e930f41e73b970ab8c08d05',1,'tgui::Label::HorizontalAlignment'],['../namespacetgui.html#a530165567f6d7efbdbc10906b81d4758',1,'tgui::HorizontalAlignment']]],
+  ['horizontallayout_17',['HorizontalLayout',['../classtgui_1_1_horizontal_layout.html',1,'tgui']]],
+  ['horizontalwrap_18',['HorizontalWrap',['../classtgui_1_1_horizontal_wrap.html',1,'tgui']]]
+];
